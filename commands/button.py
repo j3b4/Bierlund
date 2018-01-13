e25f5f89774826aef81afd43cc5a5c621054164f
Joshua <joshua@bearsend.ca>
2018-01-13 01:09:34 -0400

    Added teleport buttons, can be set to particular destination by bulders.

--- commands/button.py
+++ commands/button.py
@@ -3,6 +3,7 @@ Defines the basic commands on a switch or buttons
 """
 
 from evennia import Command, CmdSet
+from evennia import default_cmds
 
 
 class CmdPush(Command):
@@ -16,12 +17,11 @@ class CmdPush(Command):
         pb
 
     This will depress the button.  Then you'll find out what
-    happens next. 
+    happens next.
     """
 
     key = "push button"
     aliases = ["push", "switch", "activate", "pb"]
-
 
     def func(self):
         """
@@ -31,11 +31,109 @@ class CmdPush(Command):
         like "teleport buttons" with a customizable teleport destination.
 
         """
-        self.caller.msg("You did it! You pushed the button.")
+        button = self.obj
+        self.caller.msg("You did it! You pushed the %s." % button.key)
+
+
+class CmdTelePush(Command):
+    """
+    Push the button. See what happens.
+
+    Usage:
+        push button
+
+    Alias:
+        pb
+
+    This will depress the button.  Then you'll find out what
+    happens next.
+    """
+
+    key = "push button"
+    aliases = ["push", "switch", "activate", "pb"]
+
+    def func(self):
+        """
+        activate the button
+        need to be able to customize the actual effect.
+        or at least create a library of effects builders can use
+        like "teleport buttons" with a customizable teleport destination.
+
+        """
+        button = self.obj
+        destination = button.db.telink
+        if not destination:
+            return
+        else:
+            room = self.caller.location
+            self.caller.msg("You did it! You pushed the %s" % button.key)
+            room.msg_contents("%s pushes the button." % self.caller)
+            self.caller.msg("You get moved to a new place!")
+            room.msg_contents("%s disapears from view!" % self.caller)
+            self.caller.move_to(destination)
+
+
+class CmdTeleLink(default_cmds.MuxCommand):
+    """
+    Links a teleport button to a particular destination.
+
+    Usage:
+        telelink <button> = <destination>
+        telelink <button>
+
+    Aliases:
+        tlink, tl
+    """
+
+    key = "telelink"
+    aliases = ["tlink", "tl"]
+    locks = "cmd:perm(link) or perm(Builder)"
+    help_category = "Building"
+
+    def func(self):
+        self.caller.msg("You used telelink")
+        caller = self.caller
+
+        if not self.args:
+            caller.msg("Usage: telelink <object> = <target>")
+            return
+
+        object_name = self.lhs
+
+        # get object
+        obj = caller.search(object_name, global_search=True)
+        if not obj:
+            return
+
+        #
+        # this is messed up bc it will try to link other obj in room
+
+        if self.rhs:
+            # this means target supplied
+            target = caller.search(self.rhs, global_search=True)
+            if not target:
+                return
+
+            string = ""
+            obj.db.telink = target
+            string += "Pushing %s will teleport player to %s" % (
+                obj.name, target.name)
+
+        elif self.rhs is None:
+            dest = obj.db.telink
+            if dest:
+                string = "%s teleports players to %s" % (obj.name,
+                                                         dest.name)
+            else:
+                string = "%s is not linked to teleport anywhere." % obj.name
+
+        caller.msg(string.strip())
+
 
 # -----------------
-# Command Set
+# Command Sets
 # -----------------
+
 
 class ButtonCmdSet(CmdSet):
 
@@ -44,3 +142,16 @@ class ButtonCmdSet(CmdSet):
 
     def at_cmdset_creation(self):
         self.add(CmdPush())
+
+
+class TeleSwitchCmdSet(CmdSet):
+    """
+    Command set that teleports the button pusher to the button's destination
+    """
+    key = "TeleSwitchCommands"
+    mergetype = "Union"
+
+    def at_cmdset_creation(self):
+        self.add(CmdTelePush())
+        self.add(CmdTeleLink())
+    pass
--- typeclasses/button.py
+++ typeclasses/button.py
@@ -1,11 +1,12 @@
 """
-This is a basic button or switch object. It has a single command in its cmdset
-if a character activates that command a single effect takes place.
+For now, this contains a set of buttons which do different things.
 """
 
 from evennia import DefaultObject
+from commands.button import ButtonCmdSet
+from commands.button import TeleSwitchCmdSet
 
-# 
+#
 # Definition of the button
 #
 
@@ -21,14 +22,32 @@ class BasicSwitch(DefaultObject):
         This function is called when creating a switch
         """
 
-    desc = "A basic switch or button"
-    desc += "You can \"push\" it."
-    self.db.desc = desc
+        desc = "A basic switch or button"
+        desc += "You can \"push\" it."
+        self.db.desc = desc
 
-    # Might be lockable
+        # Might be lockable
 
-    self.db.locked = False
+        self.db.locked = False
 
-    self.cmdset.add_default(CmdSetSwitch)
+        self.cmdset.add_default(ButtonCmdSet)
 
 
+class TeleSwitch(BasicSwitch):
+    """
+    My idea is that this inherits the fatueres of the basic switch but
+    specifically teleports the character pushing the button/switch to
+    a destination. "destination" is settable by the builder.
+
+    problem is, its the command set that needs to be different.
+    """
+
+    def at_object_creation(self):
+        super(TeleSwitch, self).at_object_creation()
+
+        # new features added
+        # first create a property called destination
+        self.cmdset.add_default(TeleSwitchCmdSet)
+
+
+# not blank and end
